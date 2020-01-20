1bcdc77c59c3510c15a54401de18296ab33b295f
ikemoti <ikepon413@icloud.com>
2020-01-20 21:45:03 +0900

    Initial Commit

---
+++ tableViewBasic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewBasic
+//
+//  Created by USER on 2020/01/20.
+//  Copyright © 2020 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
